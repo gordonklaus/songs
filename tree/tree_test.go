1acb803f66adc1d134f0fcd0ed8f0fbf12abdc93
Gordon Klaus <gordon.klaus@telenordigital.com>
2015-12-20 16:26:26 +0100

    tree:  Added benchmark for melody.

---
+++ tree/tree_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func BenchmarkMelody(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+
+	m := newMelody(1, 8)
+	// for i := 0; i < 400; i++ {
+	// 	m.next(allRats)
+	// }
+	// b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.time += .25
+		m.next(allRats)
+	}
+	fmt.Println(m.history[len(m.history)-1].n)
+}
